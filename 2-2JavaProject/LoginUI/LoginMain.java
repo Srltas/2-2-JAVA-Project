14db28c851546e50b96e3534a11a487eeaf71278
kdm97 <kdm97@DESKTOP-12STD32>
2019-11-15 14:30:38 +0900

    다시 시작!

---
+++ 2-2JavaProject/LoginUI/LoginMain.java
@@ -0,0 +1,35 @@
+package LoginUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class LoginMain extends Application {
+	
+	@FXML
+	static private AnchorPane mainView;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			LoginUIClient client = new LoginUIClient();
+			client.startClient();
+			
+			primaryStage.setTitle("끝말잇기 게임");
+			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("LoginUI/Login.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
