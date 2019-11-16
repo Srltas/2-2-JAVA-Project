61bf7167575746fc315254cbffc0dd4d3a8a1f59
KangBu Jeong <Kang-dot@user.github.com>
2019-11-17 03:00:20 +0900

    공백을 없애면서 글자수 제한을 거는 기능 추가

--- 2-2JavaProject/Login/CreateAccountService.java
+++ 2-2JavaProject/Login/CreateAccountService.java
@@ -2,10 +2,14 @@ package Login;
 
 public class CreateAccountService {
 	public boolean createAccount(String id, String password, String phoneNumber) {
-		if(id != null && password != null) {
-			boolean result = new AccountMapper().createAccount(id, password, phoneNumber);
-			if (result != false) {
-				return true;
+		if (id != null && password != null) {
+			id = id.replace(" ", "");
+			password = password.replace(" ","");
+			if ((id.length() >= 6 && id.length() <= 20) && (password.length() >= 7 && password.length() <= 20)) {
+				boolean result = new AccountMapper().createAccount(id, password, phoneNumber);
+				if (result != false) {
+					return true;
+				}
 			}
 		}
 		return false;
