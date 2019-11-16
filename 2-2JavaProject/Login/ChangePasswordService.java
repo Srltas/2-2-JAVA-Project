bdb3888acbadacfa9bf224a4d92942def7a81a46
KangBu Jeong <Kang-dot@user.github.com>
2019-11-17 03:00:54 +0900

    기존 SRS 계획서에 따라 비밀번호를 두번 입력 받아서 같을 경우 변경함

--- 2-2JavaProject/Login/ChangePasswordService.java
+++ 2-2JavaProject/Login/ChangePasswordService.java
@@ -1,11 +1,13 @@
 package Login;
 
 public class ChangePasswordService {
-	//public boolean changePassword(String id, String password)
-	public boolean changePassword(String id, String password) {
-		if ((id != null) && (password != null)) {
-			new AccountMapper().changePasswordById(id, password);
-			return true;
+	// public boolean changePassword(String id, String password)
+	public boolean changePassword(String id, String firstPasswordInput, String secondPasswordInput) {
+		if ((firstPasswordInput.equals(secondPasswordInput))) {
+			if ((id != null) && (firstPasswordInput != null)) {
+				new AccountMapper().changePasswordById(id, firstPasswordInput);
+				return true;
+			}
 		}
 		return false;
 	}
