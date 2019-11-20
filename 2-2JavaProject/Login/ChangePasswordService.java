6628bb75ef9a09e0e71ce20168448ee5a2ba049a
KangBu Jeong <Kang-dot@user.github.com>
2019-11-20 17:50:25 +0900

    비밀번호를 두번 입력 받게 하는 수정

--- 2-2JavaProject/Login/ChangePasswordService.java
+++ 2-2JavaProject/Login/ChangePasswordService.java
@@ -1,11 +1,12 @@
 package Login;
 
 public class ChangePasswordService {
-	//public boolean changePassword(String id, String password)
-	public boolean changePassword(String id, String password) {
-		if ((id != null) && (password != null)) {
-			new AccountMapper().changePasswordById(id, password);
-			return true;
+	public boolean changePassword(String id, String password, String password1) {
+		if ((id != null) && (password != null) && (password1 != null)) {
+			if (password.equals(password1)) {
+				new AccountMapper().changePasswordById(id, password);
+				return true;
+			}
 		}
 		return false;
 	}
